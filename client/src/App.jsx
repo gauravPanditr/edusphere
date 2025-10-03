a39a220349c4f6a1ed1f33337fc252c9667b2057
gauravPanditr <gauravpandit7856@gmail.com>
2025-10-03 20:45:17 +0530

    creating routes pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/student/Home'
+import CourseList from './pages/student/CourseList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollment from './pages/student/MyEnrollment'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/course-list' element={<CourseList/>}/>
+        <Route path='/course/:id' element={<CourseDetails />}/>
+        <Route path='/my-enrollment' element={<MyEnrollment />}/>
+         <Route path='/player/:courseId' element={<MyEnrollment />}/>
+          <Route path='/loading/:path' element={<MyEnrollment />}/>
+        
+      </Routes>
     </div>
   )
 }
