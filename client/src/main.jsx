2b93c2d2b0395a9a09dc3de9c5e8037dec0c0344
gauravPanditr <gauravpandit7856@gmail.com>
2025-10-05 11:11:25 +0530

    fixing routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,41 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+
 import { AppContextProvider } from './context/AppContext.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/student/Home'
+import CourseList from './pages/student/CourseList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollment from './pages/student/MyEnrollment'
+import Educator from './pages/educator/Educator.jsx'
+import Dashboard from './pages/educator/Dashboard.jsx'
+import AddCourse from './pages/educator/AddCourse.jsx'
+import MyCourse from './pages/educator/MyCourse.jsx'
+import StudenEnroll from './pages/educator/StudenEnroll.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <AppContextProvider>
-    <App />
-  </AppContextProvider>,
-  </BrowserRouter>
-  
+  <StrictMode>
+    <BrowserRouter>
+      
+        <Routes>
+          {/* Default redirect to /home */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/course-list" element={<CourseList />} />
+          <Route path="/course/:id" element={<CourseDetails />} />
+          <Route path="/my-enrollment" element={<MyEnrollment />} />
+          <Route path="/player/:courseId" element={<MyEnrollment />} />
+          <Route path="/loading/:path" element={<MyEnrollment />} />
+             <Route path="/educator" element={<Educator />}>
+            <Route index element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="add-course" element={<AddCourse />} />
+            <Route path="my-courses" element={<MyCourse />} />
+            <Route path="student-enrolled" element={<StudenEnroll />} />
+          </Route>
+        </Routes>
+    
+    </BrowserRouter>
+  </StrictMode>
 )
