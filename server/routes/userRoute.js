eb1c1ffad8b829990b35ed1224d8386606e1cc4c
gauravPanditr <gauravpandit7856@gmail.com>
2025-11-03 12:12:52 +0530

    making course progress

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { addUserRating, getUserCourseProgress, getUserData, updateUserCourseProgress, userEnrolledCourses } from "../controllers/userController";
 
 const userRouter=express.Router();
 
 userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
+userRouter.post("/update-course-progress", updateUserCourseProgress);
+userRouter.post("/get-course-progress", getUserCourseProgress);
+userRouter.post("/add-rating", addUserRating);
 
 export default userRouter;
